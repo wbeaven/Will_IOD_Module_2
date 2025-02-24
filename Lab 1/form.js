8732957dc4d76d4ceaa74b681856ad1bc72b38b8
Will-remote <wtbeaven@gmail.com>
2025-02-24 21:09:29 +1100

    Edited lab 1 to get form data

---
+++ Lab 1/form.js
@@ -0,0 +1,41 @@
+function onFormSubmit(e) {
+  e.preventDefault();
+  const formattedObj = getFormattedData(e);
+  console.log("formattedObj", formattedObj);
+  console.log("Data", getData())
+  fetch("http://localhost:3000/profile", {
+    method: "POST",
+    body: JSON.stringify(formattedObj),
+  });
+}
+
+function getFormattedData(e) {
+  const formData = new FormData(e.target);
+  const formObj = Object.fromEntries(formData.entries());
+  const selectedElements = document.querySelectorAll(
+    "input[type=checkbox][name=car_type]:checked"
+  );
+  const vals = Array.from(selectedElements).map((e) => e.value);
+  formObj.carType = vals;
+  return formObj;
+} // car type is doubled up. one is an array and the other is the last selected value.
+
+function getData() {
+  const formData = {};
+  const firstName = document.getElementById("fName").value;
+  formData.firstName = firstName;
+  const lastName = document.getElementById("lName").value;
+  formData.lastName = lastName;
+  const favLang = document.querySelector(
+    "input[type=radio][name=fav_lang]:checked"
+  ).value;
+  formData.favLang = favLang;
+  const selectedElements = document.querySelectorAll(
+    "input[type=checkbox][name=car_type]:checked"
+  );
+  const vals = Array.from(selectedElements).map((e) => e.value);
+  formData.carType = vals;
+  const carBrand = document.getElementById("car_brand").value;
+  formData.carBrand = carBrand;
+  return formData;
+}
